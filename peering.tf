7225349168fda85acfb37cde3aa24310f29a8a30
azharmd-dev <azharmd0794@gmail.com>
2026-02-17 19:28:24 +0530

    updated data and peering connection

--- peering.tf
+++ peering.tf
@@ -24,10 +24,22 @@ resource "aws_vpc_peering_connection" "default" {
 
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
+
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+
+  depends_on = [
+    aws_vpc_peering_connection.default
+  ]
 }
+
+# resource "aws_route" "public_peering" {
+#   count = var.is_peering_required ? 1 : 0
+#   route_table_id            = aws_route_table.public.id
+#   destination_cidr_block    = data.aws_vpc.default.cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+# }
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
@@ -42,4 +54,9 @@ resource "aws_route" "private_peering" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+
+  depends_on = [
+    aws_vpc_peering_connection.default
+  ]
 }
+
